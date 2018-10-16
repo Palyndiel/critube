6c804356214c03fe50d2e460a9bc196a3aa2f886
Maxime Minot <maxym.mino@gmail.com>
2018-10-16 21:42:40 +0200

    Modification des paramètres et update

--- app/config/parameters_prod.php
+++ app/config/parameters_prod.php
@@ -11,5 +11,5 @@ $container->setParameter('secret', getenv('SECRET'));
 $container->setParameter('locale', 'en');
 $container->setParameter('mailer_transport', null);
 $container->setParameter('mailer_host', null);
-$container->setParameter('mailer_user', null);
+$container->setParameter('mailer_user', 'admin@mail.com');
 $container->setParameter('mailer_password', null);
